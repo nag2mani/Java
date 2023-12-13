3e87e3132288fd5996401594d92d89941a6043ac
Nagmani kumar <nag2mani@gmail.com>
2023-12-13 10:28:54 +0530

    printing pattern

--- Sem_3_Practice.java
+++ Sem_3_Practice.java
@@ -383,3 +383,15 @@ import java.io.IOException;
 //         scnr.close();
 //     }
 // }
+
+
+class Sem_3_Practice{
+    public static void main(String[] args){
+        for (int i =1 ; i<= 5; i++){
+            for (int j=1; j<=i;j++){
+                
+            }
+        }
+        System.out.println();
+    }
+}
