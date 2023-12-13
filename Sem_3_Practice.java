460226f36e55a3dd9f9dda584b3c8d9a86361f3a
Nagmani kumar <nag2mani@gmail.com>
2023-12-13 10:48:09 +0530

    printing pattern1

--- Sem_3_Practice.java
+++ Sem_3_Practice.java
@@ -387,11 +387,18 @@ import java.io.IOException;
 
 class Sem_3_Practice{
     public static void main(String[] args){
+        int j, k, l;
         for (int i =1 ; i<= 5; i++){
-            for (int j=1; j<=i;j++){
-                
+            for (j=1; j<=i;j++){
+                System.out.print(j+"@");
             }
+            for (k=1; k<j;k++){
+                System.out.print(j-1 +"&");
+            }
+            for (l=j-2; l>=1;l--){
+                System.out.print(l+"#");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 }
