6b4acf12d33fdd8b2f4d092ac61df12130e00de8
Nagmani kumar <nag2mani@gmail.com>
2023-12-14 11:02:38 +0530

    pascal triangle left

--- Sem_3_Practice.java
+++ Sem_3_Practice.java
@@ -463,54 +463,45 @@ import java.io.IOException;
 //             }
 //             count++;
 //             System.out.println();
-            
+
 //         }
 //     }
 // }
 
-
 public class Sem_3_Practice {
-  public static void main(String[] args)
-  {
-    //This represents the row with the max stars
-    int numberOfRows;
-    System.out.println("Enter the number of rows: ");
-    //Getting the number of rows from user
-    Scanner sc = new Scanner(System.in);
-    numberOfRows = sc.nextInt();
+    public static void main(String[] args) {
+        // This represents the row with the max stars
+        int numberOfRows;
+        System.out.println("Enter the number of rows: ");
+        // Getting the number of rows from user
+        Scanner sc = new Scanner(System.in);
+        numberOfRows = sc.nextInt();
 
-    //This is Outer Loop 1: This prints the first half of
-    // the pattern
-    for (int i= 1; i<=numberOfRows; i++)
-    {
-      //Prints the spaces before the first star of each row
-      for (int j=i; j<numberOfRows; j++)
-      {
-        System.out.print(" ");
-      }
-      //Prints the stars for each row
-      for (int k=1; k<=i;k++)
-      {
-        System.out.print("*");
-      }
-      //To move the cursor to new line after printing row
-      System.out.println();
+        for (int i = 1; i <= numberOfRows; i++) {
+            // Prints the spaces before the first star of each row
+            for (int j = i; j < numberOfRows; j++) {
+                System.out.print(" ");
+            }
+            // Prints the stars for each row
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+        for (int i = numberOfRows; i >= 1; i--) {
+            // Prints the whitespaces before the first star of each row
+            for (int j = i; j <= numberOfRows; j++) {
+                System.out.print(" ");
+            }
+            // Prints stars of each row
+            for (int k = 1; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
-    //Outer Loop 2: Prints second half of the triangle
-    for (int i=numberOfRows; i>=1; i--)
-    {
-      //Prints the whitespaces before the first star of each row
-      for(int j=i; j <=numberOfRows;j++)
-      {
-        System.out.print(" ");
-      }
-      //Prints stars of each row
-      for(int k=1; k<i ;k++)
-      {
-        System.out.print("*");
-      }
-      //To move the cursor to new line after printing row
-      System.out.println();
-    }
-  }
 }
+
