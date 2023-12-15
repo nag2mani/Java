8a3c09a13440be5651287bc713c5f080faa99667
Nagmani kumar <nag2mani@gmail.com>
2023-12-15 23:34:14 +0530

    java oops c-2

--- IA_02_Hashmap.java
+++ IA_02_Hashmap.java
@@ -10,21 +10,21 @@ public class IA_02_Hashmap {
         d.put("soapyy", 200);
         d.put("water", 15);
         d.put("sanitiser", 50);
-        // for (String i: d.keySet()){
-        //     System.out.println("item :"+ i +", value :" + d.get(i));
-        // }
-        // ArrayList<Integer> array = new ArrayList<>();
-        // for (String i: d.keySet()){
-        //     int x = d.get(i);
-        //     array.add(x);
-        // }
-        // Collections.sort(array);
-        // System.out.println(array);
-        // System.out.println(d);
+        for (String i: d.keySet()){
+            System.out.println("item :"+ i +", value :" + d.get(i));
+        }
+        ArrayList<Integer> array = new ArrayList<>();
+        for (String i: d.keySet()){
+            int x = d.get(i);
+            array.add(x);
+        }
+        Collections.sort(array);
+        System.out.println(array);
+        System.out.println(d);
 
-        // for (int i; i<array.size(); i++){
-        //     System.out.println("item :"+ i +", value :" + d.get(i));
-        // }
+        for (int i; i<array.size(); i++){
+            System.out.println("item :"+ i +", value :" + d.get(i));
+        }
 
         for(String i : d.keySet()){
             System.out.println(d.get(i));
