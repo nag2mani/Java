29aad6b70f9520c7f0b3a2a552a93d2ea8e95170
nag2mani <nag2mani@gmail.com>
2024-01-24 13:55:19 +0530

    swing code part1 done

--- sem4/swing/swing_code.java
+++ sem4/swing/swing_code.java
@@ -3,8 +3,11 @@ import javax.swing.*;
 public class swing_code {
     public static void main(String[] args) {
         JFrame f = new JFrame();
-        JButton b = new JButton("click Here");
+        JButton b = new JButton("Click");
         b.setBounds(130, 100, 100, 40); //x-axis, y-axis, width and height.
         f.add(b);
+        f.setSize(1000, 500); //size of the frame.
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
