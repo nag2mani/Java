dc691cd76dd886b7679a0362f3131702aafc8e30
nag2mani <nag2mani@gmail.com>
2024-01-24 13:42:56 +0530

    ass1

---
+++ sem4/swing/swing_code.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class swing_code {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        JButton b = new JButton("click Here");
+        b.setBounds(130, 100, 100, 40);
+        f.add(b);
+    }
+}
