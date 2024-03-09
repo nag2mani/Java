5a02fed9e322527d098a9f93ecc8febf8b635adc
nag2mani <nag2mani@gmail.com>
2024-03-09 16:09:58 +0530

    Binary search done

--- sem4/assignment_package/Searching/BinarySearch.java
+++ sem4/assignment_package/Searching/BinarySearch.java
@@ -1,17 +1,17 @@
 package Searching;
 
 
-class BinarySearch {
+public class BinarySearch {
 
     //binary search method.
-    int binarySearch(int arr[], int l, int r, int x) {
+    public static int binary_search(int arr[], int l, int r, int x) {
         if (r >= l) {
             int mid = l + (r - l) / 2;
             if (arr[mid] == x)
                 return mid;
             if (arr[mid] > x)
-                return binarySearch(arr, l, mid - 1, x);
-            return binarySearch(arr, mid + 1, r, x);
+                return binary_search(arr, l, mid - 1, x);
+            return binary_search(arr, mid + 1, r, x);
         }
         return -1;
     }
