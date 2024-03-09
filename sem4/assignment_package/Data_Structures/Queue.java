797898996433ced6c44a142bdd3853aa3bd8edf0
nag2mani <nag2mani@gmail.com>
2024-03-09 15:29:55 +0530

    Data Structure done

--- sem4/assignment_package/Data_Structures/Queue.java
+++ sem4/assignment_package/Data_Structures/Queue.java
@@ -1,5 +1,6 @@
 package Data_Structures;
 
+
 public class Queue {
 
     int SIZE = 5;
@@ -27,8 +28,9 @@ public class Queue {
             return false;
     }
 
+
     // insert elements to the queue
-    void enQueue(int element) {
+    void enqueue(int element) {
         // if queue is full
         if (isFull()) {
             System.out.println("Queue is full");
@@ -44,9 +46,9 @@ public class Queue {
         }
     }
 
-    // delete element from the queue
-    int deQueue() {
 
+    // delete element from the queue
+    int dequeue() {
         int element;
         // if queue is empty
         if (isEmpty()) {
@@ -68,6 +70,7 @@ public class Queue {
         }
     }
 
+
     // display element of the queue
     void display() {
         int i;
@@ -85,88 +88,3 @@ public class Queue {
         }
     }
 }
-
-// // Program 2:
-// public class Queue {
-// int SIZE = 5;
-// int items[] = new int[SIZE];
-// int front, rear;
-
-// Queue() {
-// front = -1;
-// rear = -1;
-// }
-
-// // check if the queue is full
-// boolean isFull() {
-// if (front == 0 && rear == SIZE - 1) {
-// return true;
-// }
-// return false;
-// }
-
-// // check if the queue is empty
-// boolean isEmpty() {
-// if (front == -1)
-// return true;
-// else
-// return false;
-// }
-
-// // insert elements to the queue
-// void enQueue(int element) {
-// // if queue is full
-// if (isFull()) {
-// System.out.println("Queue is full");
-// } else {
-// if (front == -1) {
-// // mark front denote first element of queue
-// front = 0;
-// }
-// rear++;
-// // insert element at the rear
-// items[rear] = element;
-// System.out.println("Insert " + element);
-// }
-// }
-
-// // delete element from the queue
-// int deQueue() {
-// int element;
-// // if queue is empty
-// if (isEmpty()) {
-// System.out.println("Queue is empty");
-
-// return (-1);
-// } else {
-// // remove element from the front of queue
-// element = items[front];
-// // if the queue has only one element
-// if (front >= rear) {
-// front = -1;
-// rear = -1;
-// } else {
-// // mark next element as the front
-// front++;
-// }
-// System.out.println(element + " Deleted");
-// return (element);
-// }
-// }
-
-// // display element of the queue
-// void display() {
-// int i;
-// if (isEmpty()) {
-// System.out.println("Empty Queue");
-// } else {
-// // display the front of the queue
-// System.out.println("\nFront index-> " + front);
-// // display element of the queue
-// System.out.println("Items -> ");
-// for (i = front; i <= rear; i++)
-// System.out.print(items[i] + " ");
-// // display the rear of the queue
-// System.out.println("\nRear index-> " + rear);
-// }
-// }
