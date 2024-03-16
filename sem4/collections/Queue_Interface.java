05681ee727b5c3ab78afc42c060e3e677c018c19
nag2mani <nag2mani@gmail.com>
2024-03-16 17:11:08 +0530

    Queue interface done

---
+++ sem4/collections/Queue_Interface.java
@@ -0,0 +1,46 @@
+//List Interfaces
+import java.util.*;
+
+
+public class Queue_Interface {
+    public static void main(String[] args) {
+        
+        Queue<String> queue = new PriorityQueue<>();
+
+        queue.add("Amit Sharma");
+        queue.add("Vijay Raj");
+        queue.add("Nagmani");
+        queue.add("JaiShankar");
+        queue.add("AaiShankar");
+        queue.add("Raj");
+
+        //Both return head.
+        // System.out.println(queue.element());
+        // System.out.println(queue.peek());
+
+        // for (String ele : queue) {
+        //     System.out.println(ele);
+        // }
+
+        Iterator<String> itr = queue.iterator();
+
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        //remove last element
+        queue.remove();
+        //remove last element
+        queue.poll();
+
+        System.out.println("after removing two elements:");
+
+        Iterator<String> itr2=queue.iterator();
+
+        while(itr2.hasNext()){ 
+            System.out.println(itr2.next()); 
+        } 
+
+        // Deque<Integer> dq = new ArrayDeque<>();
+     }
+}
