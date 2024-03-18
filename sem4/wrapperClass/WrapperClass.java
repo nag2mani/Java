0b7b73d5440e80041dcd112f0fa1bf67cf77bc52
nag2mani <nag2mani@gmail.com>
2024-03-18 17:06:47 +0530

    wrapper class done

--- sem4/wrapperClass/WrapperClass.java
+++ sem4/wrapperClass/WrapperClass.java
@@ -1,5 +1,18 @@
 public class WrapperClass {
     public static void main(String[] args) {
-        
+
+        //Autoboxing Example.
+        int a = 22;
+        Integer b = Integer.valueOf(a); //Explicitly converting into Integer
+        Integer c = a; //autoboxing.
+        System.out.println(a+ " " + b + " " + c);
+
+
+        //Unboxing Example
+        // Integer i = new Integer(55);
+        Integer i = 56;
+        int j = i.intValue(); //Explicitly converting into Integer
+        int k = i; //Unboxing.
+        System.out.println(i+ " " + j + " " + k);
     }
 }
