dfaf3e4cf3e1f0a6f38cb2dabcf4431eb4f7c345
nag2mani <nag2mani@gmail.com>
2024-04-23 11:52:20 +0530

    completed hashtable

--- Advance Java/hashTable/HashTable3.java
+++ Advance Java/hashTable/HashTable3.java
@@ -23,19 +23,19 @@ public class HashTable3 {
 
         // Creating Books
         Book b1 = new Book(101, "Let us C", "Yashwant Kanetkar", "BPB", 8);
-        Book b2 = new Book(102, "Data Communications & Networking", "Forouzan", "McGraw Hill", 4);
+        Book b2 = new Book(105, "Data Communications & Networking", "Forouzan", "McGraw Hill", 4);
         Book b3 = new Book(103, "Operating System", "Galvin", "Wiley", 6);
 
         // Adding Books to map
         map.put(1, b1);
-        map.put(2, b2);
         map.put(3, b3);
+        map.put(2, b2);
 
         // Traversing map
         for (Map.Entry<Integer, Book> entry : map.entrySet()) {
             int key = entry.getKey();
             Book b = entry.getValue();
-            System.out.println(key + " Details:");
+            System.out.println(" Details: " + key);
             System.out.println(b.id + " " + b.name + " " + b.author + " " + b.publisher + " " + b.quantity);
             System.err.println();
         }
