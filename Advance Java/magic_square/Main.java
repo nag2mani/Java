8d76d64608cf3ed87085379f1755f424a6d89245
nag2mani <nag2mani@gmail.com>
2024-04-27 21:35:56 +0530

    done

--- Advance Java/magic_square/Main.java
+++ Advance Java/magic_square/Main.java
@@ -1,22 +1,28 @@
 public class Main {
+
+    // Generate the matrix and print all the required matrix.
     public static void main(String[] args) {
-        int[][] matrix = generateMatrix();
-        printMatrix(matrix);
+        int[][] requiredMatrix = generateAllMatrix();
+        printAllMatrix(requiredMatrix);
     }
 
-    public static int[][] generateMatrix() {
-        int[][] matrix = new int[3][3];
+
+    // Function to generate the original magic square matrix
+    public static int[][] generateAllMatrix() {
+        int[][] requiredMatrix = new int[3][3];
         int n = 3;
         int row = 0;
         int col = n / 2;
         int num = 1;
 
+        // Loop to fill the matrix with numbers in a magic square pattern
         while (num <= n * n) {
-            matrix[row][col] = num;
+            requiredMatrix[row][col] = num;
             num++;
             row--;
             col++;
 
+            // Handle wrapping around if going out of bounds
             if (row < 0 && col == n) {
                 row = 1;
                 col = n - 1;
@@ -24,15 +30,16 @@ public class Main {
                 row = n - 1;
             } else if (col == n) {
                 col = 0;
-            } else if (matrix[row][col] != 0) {
+            } else if (requiredMatrix[row][col] != 0) {
                 row += 2;
                 col--;
             }
         }
-
-        return matrix;
+        return requiredMatrix;
     }
 
+
+    // Function to print a matrix
     public static void printMatrix(int[][] matrix) {
         for (int[] row : matrix) {
             for (int cell : row) {
@@ -41,4 +48,32 @@ public class Main {
             System.out.println();
         }
     }
+
+
+    // Function to print the original and rotated matrices
+    public static void printAllMatrix(int[][] matrix) {
+        System.out.println("First Matrix: ");
+        printMatrix(matrix);
+        System.out.println("Second Matrix: ");
+        printMatrix(rotateMatrixClockwise(matrix));
+        System.out.println("Third Matrix: ");
+        printMatrix(rotateMatrixClockwise(rotateMatrixClockwise(matrix)));
+        System.out.println("Fourth Matrix: ");
+        printMatrix(rotateMatrixClockwise(rotateMatrixClockwise(rotateMatrixClockwise(matrix))));
+    }
+
+
+    // Function to rotate a matrix clockwise by 90 degrees
+    public static int[][] rotateMatrixClockwise(int[][] matrix) {
+        int n = matrix.length;
+        int[][] rotatedMatrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotatedMatrix[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        return rotatedMatrix;
+    }
+
 }
+
